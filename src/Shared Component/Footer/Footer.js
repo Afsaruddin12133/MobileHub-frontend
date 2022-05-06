f53e68a8c9fed3afc628042f17094c72bd42426c
Afsaruddin12133 <afsaruddin12133@gmail.com>
2022-05-07 00:34:31 +0600

    update navigation & footer

--- src/Shared Component/Footer/Footer.js
+++ src/Shared Component/Footer/Footer.js
@@ -2,9 +2,75 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="text-center bg-gray-900 text-white">
-        <div className="container px-6 pt-6">
-          <div className="flex justify-center mb-6">
+        <div className="text-center bg-gray-900 text-white p-10 pb-0">
+         <div className='md:flex md:justify-around md:gap-10'>
+          {/* Prodcut section  */}
+         <div className="">
+        <h6 className="uppercase font-semibold mb-4 text-2xl  text-center md:text-xl ">
+          Products
+        </h6>
+        <p className="mb-4">
+          <a href="#!" className="text-white">Angular</a>
+        </p>
+        <p className="mb-4">
+          <a href="#!" class="text-white">React</a>
+        </p>
+        <p className="mb-4">
+          <a href="#!" className="text-white">Vue</a>
+        </p>
+        <p>
+          <a href="#!" className="text-white">Laravel</a>
+        </p>
+      </div>
+       {/* contact section  */}
+          <div className="">
+        <h6 className="uppercase font-semibold mt-8 mb-4 text-2xl text-center md: flex md:mt-0 md:text-xl">
+          Contact
+        </h6>
+        <p className="flex items-center justify-center md:justify-start mb-4">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home"
+            className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path fill="currentColor"
+              d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z">
+            </path>
+          </svg>
+          New York, NY 10012, US
+        </p>
+        <p className="flex items-center justify-center md:justify-start mb-4">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope"
+            className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512">
+            <path fill="currentColor"
+              d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z">
+            </path>
+          </svg>
+          info@example.com
+        </p>
+        <p className="flex items-center justify-center md:justify-start mb-4">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone"
+            className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512">
+            <path fill="currentColor"
+              d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z">
+            </path>
+          </svg>
+          + 01 234 567 88
+        </p>
+        <p className="flex items-center justify-center md:justify-start">
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="print"
+            className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512">
+            <path fill="currentColor"
+              d="M448 192V77.25c0-8.49-3.37-16.62-9.37-22.63L393.37 9.37c-6-6-14.14-9.37-22.63-9.37H96C78.33 0 64 14.33 64 32v160c-35.35 0-64 28.65-64 64v112c0 8.84 7.16 16 16 16h48v96c0 17.67 14.33 32 32 32h320c17.67 0 32-14.33 32-32v-96h48c8.84 0 16-7.16 16-16V256c0-35.35-28.65-64-64-64zm-64 256H128v-96h256v96zm0-224H128V64h192v48c0 8.84 7.16 16 16 16h48v96zm48 72c-13.25 0-24-10.75-24-24 0-13.26 10.75-24 24-24s24 10.74 24 24c0 13.25-10.75 24-24 24z">
+            </path>
+          </svg>
+          + 01 234 567 89
+        </p>
+      </div>
+    {/* social media section  */}
+      <div className="px-6 pt-6">
+      <h4>Get connected with us on social networks:</h4>
+          <div className="flex  mb-6">
             <a href="#!" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
               <svg aria-hidden="true"
               focusable="false"
@@ -38,7 +104,6 @@ const Footer = () => {
                 ></path>
               </svg>
             </a>
-      
             <a href="#!" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
               <svg aria-hidden="true"
               focusable="false"
@@ -55,12 +120,9 @@ const Footer = () => {
                 ></path>
               </svg>
             </a>
-      
-      
           </div>
-
         </div>
-      
+         </div>
         <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
           2021  © Copyright
           <a className="text-white" href="https://tailwind-elements.com/"> MobileHub</a>
--- src/Shared Component/Navigation/Nav.js
+++ src/Shared Component/Navigation/Nav.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import img from '../../Component/photo/Mobilehub_Logo.png';
 
 const Nav = () => {
   const activestyle=({ isActive }) => {
     return {
+      margin:"10px",
       display: "block",
-      margin: "1rem 0",
+      margin: "8px 0",
       textDecoration : "none",
-      color: isActive ? "#00B4D8" : "white",
+      color: isActive ? "#6FDFDF" : "white",
     };
   };
   const navinfo = [
@@ -20,17 +22,58 @@ const Nav = () => {
       {id:7,name:"About Us",link:"/about"},
   ]
     return (
-        <nav className='md:flex  bg-[#03045E] md:justify-between md:px-8 flex'>
-           <div>
-             {/* <img src={} alt="" className='cursor-pointer' /> */}
-          
-           </div>
-           <div className='md:flex md:gap-8 md:p-6  md:block hidden'>
-           {
-               navinfo.map(info =>  <NavLink key={info.id}  to = {info.link} style={activestyle}>{info.name}</NavLink>)
-           }
-           </div>
-        </nav>
+      
+  <nav className="
+  sticky  
+  top-0
+  py-8
+  md:py-0
+  w-full
+  flex flex-wrap
+  items-center
+  justify-between
+  bg-[#5534A5]
+  text-gray-500
+  hover:text-gray-700
+  focus:text-gray-700
+  shadow-lg
+  navbar navbar-expand-lg navbar-light
+  ">
+  <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+  <button className="
+      navbar-toggler
+      text-gray-500
+      border-0
+      hover:shadow-none hover:no-underline
+      py-2
+      px-2.5
+      bg-transparent
+      focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline
+    " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
+    className="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+    <path fill="currentColor"
+      d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+    </path>
+  </svg>
+  </button>
+  <div className="collapse navbar-collapse flex-grow items-center justify-between" id="navbarSupportedContent">
+  <div className="container-fluid">
+        <a className="flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mt-2 lg:mt-0 mr-1" href="#">
+        <img src={img} alt="" className='cursor-pointer' />
+        </a>
+      </div>
+
+  <ul className="navbar-nav flex flex-col pl-0 list-style-none md:gap-8">
+            {
+                navinfo.map(info =>  <NavLink key={info.id}  to = {info.link} style={activestyle}>{info.name}</NavLink>)
+            }
+  </ul>
+  </div>
+  </div>
+    </nav>
+
     );
 };
 
@@ -39,6 +82,28 @@ export default Nav;
 
 
 
-// <NavLink to = "/" style={activestyle}>Home</NavLink>
-//             <NavLink to = "/blogs" style={activestyle}>Blogs</NavLink>
-//             <NavLink to = "/about" style={activestyle}>About Us</NavLink>
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+  <nav classNameName='md:flex  bg-[#5534A5] md:justify-between md:px-8 flex'>
+           <div>
+             <img src={img} alt="" classNameName='cursor-pointer' />
+          
+           </div>
+           <div classNameName='md:flex md:gap-8 md:p-6  md:block hidden'>
+           {
+               navinfo.map(info =>  <NavLink key={info.id}  to = {info.link} style={activestyle}>{info.name}</NavLink>)
+           }
+           </div>
+        </nav> */}
